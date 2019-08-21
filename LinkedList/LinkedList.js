cbf0292c37b806d3d9c1eaf40a9b89c7494759d2
amr taher <amrtaher1995@gmail.com>
2019-08-21 21:31:00 +0200

    adding extra funcitons (addNodeAt and DeleteNodeAt)

--- LinkedList/LinkedList.js
+++ LinkedList/LinkedList.js
@@ -33,10 +33,65 @@ class LinkedList {
     }
     traverseInverse() {
         let temp = this.tail;
-        while (temp !== null) {
+        while (temp) {
             console.log(temp.value);
             temp = temp.previous;
         }
+    }
+    addNodeAt(index, val) {
+        if (index > this.count || index < 0) {
+            console.log('cant insert in an index bigger than count of linked list or negative index');
+            return;
+        }
+
+        let nodeToInsert = new Node(val, null, null);
+        let i = 0;
+        let temp = this.head;
+        if (index === 0) {
+            nodeToInsert.next = this.head;
+            nodeToInsert.previous = null;
+            this.head = nodeToInsert;
+        }
+        while (temp) {
+            if (i === index - 1) {
+                nodeToInsert.next = temp.next;
+                nodeToInsert.previous = temp.previous;
+                temp.next = nodeToInsert;
+                this.count++;
+                break;
+            }
+            temp = temp.next;
+            i++;
+        }
+        this.count++;
+    }
+    deleteNodeAt(index) {
+        if (index > this.count || index < 0) {
+            console.log('cant delete index bigger than count or negative');
+            return;
+        }
+        if (this.count === 1) {
+            this.head = null;
+            this.tail = null;
+        }
+        let temp = this.head;
+        let i = 0;
+        while (temp) {
+            if (i === index) {
+                // take previous node and assign its next to the (to be deleted node) next. 20 5
+                // assign the to be deleted node next previous node to the to be deleted previous.
+                // deleting by assinging the node to null
+                temp.previous.next = temp.next;
+                if (temp.next) {
+                    temp.next.previous = temp.previous;
+                }
+                temp = null;
+                break;
+            }
+            i++;
+            temp = temp.next;
+        }
+        this.count--;
     }
 }
 let l = new LinkedList();
@@ -45,4 +100,10 @@ l.addNode(10);
 l.addNode(5);
 l.addNode(0);
 l.traverse();
-l.traverseInverse();
+console.log('..>>..');
+l.deleteNodeAt(1);
+l.deleteNodeAt(1);
+l.deleteNodeAt(1); // 20 5
+l.traverse();
+console.log('..>>..');
+// l.traverseInverse();
